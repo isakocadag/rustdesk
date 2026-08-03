b0cd0d3886f44125097791dcb4d3cede966b09cd
isakocadag <ikocadag@hotmail.com>
2026-08-03 12:55:27 +0300

    Replace About copyright

--- flutter/lib/desktop/pages/desktop_setting_page.dart
+++ flutter/lib/desktop/pages/desktop_setting_page.dart
@@ -2528,7 +2528,7 @@ class _AboutState extends State<_About> {
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           Text(
-                            'Copyright © ${DateTime.now().toString().substring(0, 4)} Purslane Tech Pte. Ltd.\n$license',
+                            'Copyright © ${DateTime.now().year} Ossis Bilişim Teknolojileri\n$license',
                             style: const TextStyle(color: Colors.white),
                           ),
                           Text(
