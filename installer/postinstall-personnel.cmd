a559211ec60aea7c56739388b9ab69be5e0e99a8
isakocadag <ikocadag@hotmail.com>
2026-08-31 17:01:54 +0300

    Start personnel app as OssisRemoteSRV

--- installer/postinstall-personnel.cmd
+++ installer/postinstall-personnel.cmd
@@ -1,5 +1,5 @@
 @echo off
-set "APP=%~dp0OssisSupportConsole.exe"
+set "APP=%~dp0OssisRemoteSRV.exe"
 set "CONFIG===Qfi0TUPRWYSVlSFx2ZoRkUG5Gc012YHdGd2h3dDJlZrVFbYJ3VwBnNHdkYDB3NiojI5V2aiwiIiojIpBXYiwiIiojI5FGblJnIsISbvNmLtl2cpxWaiNXazN3bus2clRGcsVGaiojI0N3boJye"
 
 if /I "%~1"=="config" (
