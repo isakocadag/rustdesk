7db480d12a22b835cf9bb48e9a97195f7dffb353
isakocadag <ikocadag@hotmail.com>
2026-09-01 22:41:46 +0300

    Fix personnel remember-me bridge import

--- flutter/lib/desktop/pages/ossis_personnel_gate.dart
+++ flutter/lib/desktop/pages/ossis_personnel_gate.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_hbb/common.dart';
+import 'package:flutter_hbb/models/platform_model.dart';
 import 'package:http/http.dart' as http;
 
 import 'ossis_personnel_session.dart';
